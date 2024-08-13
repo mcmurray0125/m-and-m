393e7e3a620bde283d2f91c67c7c0ce8c4ea00f3
Michael Murray <mcmurray0125@gmail.com>
2024-08-13 12:37:03 -0500

    init firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 import './styles/app.css'
 import { router } from './routes'
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router}/>
-    </>
+    </AuthProvider>
   )
 }
 
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,115 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { auth, db } from "../firebase"
+import { doc, setDoc, getDoc } from "firebase/firestore"
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInAnonymously,
+  sendPasswordResetEmail,
+  updateEmail,
+  updatePassword
+} from "firebase/auth";
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+    const [hasFolder, setHasFolder] = useState(false)
+
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    } 
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    function loginDemo() {
+      return (
+        signInAnonymously(auth)
+        )
+    }
+
+    function logout() {
+        return auth.signOut()
+    }
+
+    function resetPassword(email) {
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    function emailChange(email) {
+        return updateEmail(currentUser, email)
+      }
+    
+      function passwordChange(password) {
+        return updatePassword(currentUser, password)
+      }
+
+      
+     useEffect(() => {
+         const unsubscribe = auth.onAuthStateChanged(user => {
+           setCurrentUser(user)
+           setLoading(false)
+         })
+    
+         return unsubscribe
+       }, [])
+
+       //Check is user has a database folder. If not, creates one with their UID
+      useEffect(() => {
+       if(currentUser && !loading) {
+         const checkFolder = async () => {
+           const docRef = doc(db, "users", currentUser.uid);
+           const docSnap = await getDoc(docRef);
+           if (docSnap.exists()) {
+             setHasFolder(true);
+           } else {
+            if (currentUser.email) {
+              try {
+                await setDoc(doc(db, "users", currentUser.uid), {
+                    saved: [],
+                    comments: []
+                  })
+            } catch(error) {
+                console.log(error)
+            }
+            } else {
+              try {
+                await setDoc(doc(db, "users", currentUser.uid), {
+                    saved: [],
+                    comments: []
+                  })
+            } catch(error) {
+                console.log(error)
+            }
+            }
+           }
+         };
+         checkFolder();
+       }
+      }, [currentUser]);
+      
+    const value = { 
+        loading,
+        currentUser,
+        login,
+        loginDemo,
+        signup,
+        logout,
+        resetPassword,
+        emailChange,
+        passwordChange,
+        hasFolder
+    }
+
+  return (
+    <AuthContext.Provider value={value}>
+        {!loading && children}
+    </AuthContext.Provider>
+  )
+}
